4e7f4b968eb92217efef6c0fbde212aeae266c56
Andrew <andrewg.walker@outlook.com>
2017-05-23 17:03:10 -0500

    Humidity function now uses digital temperature, added uv measurment function

--- arduino/analog.h
+++ arduino/analog.h
@@ -5,6 +5,7 @@ String readTemp(int pin)
   float reading = analogRead(pin);
   float voltage = reading * 5.0;
   voltage /= 1024.0; 
+  
  
   float temp_c = (voltage - 0.5) * 100 ;
   float temp_f = (temp_c * 9.0 / 5.0) + 32.0;
@@ -17,8 +18,20 @@ String readHumidity(int pin)
 {
   float voltage = analogRead(pin) * 5 / 1023;
   float relative_humidity = (voltage / (.0062 * 5)) - 25.81;
-  float true_humidity = relative_humidity/(1.0546 - (0.00216 * (readTemp(TEMP_0)).toFloat()));
+  float true_humidity = relative_humidity/(1.0546 - (0.00216 * pressure_sensor.getTemperature(FAHRENHEIT, ADC_2048)));
   return String(true_humidity);
+}
+
+String readUV(int pin)
+{
+  float reading = analogRead(pin);
+  float volts = reading / (1024 * 5);
+  Serial.print("[DATA] PHOTO_0: ");
+  Serial.print(volts, 7);
+  Serial.print('\n');
+
+  return String(volts);
+  
 }
 
 

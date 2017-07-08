e3f979f0b38705f072d1a7d9b4a4ac39287bb38b
Andrew <andrewg.walker@outlook.com>
2017-07-08 11:00:14 -0500

    Added debug interface to allow for testing configurations

--- arduino/debug.h
+++ arduino/debug.h
@@ -29,3 +29,56 @@ void printBits(uint16_t myByte){
        Serial.print('0');
  }
 }
+
+
+void wait_for_rx()
+{
+  while(Serial.available() < 1){
+     delay(10);
+  }
+}
+
+int debugInterface()
+{
+  // Wait to recieve a command in rx buffwe
+  wait_for_rx();
+  
+  byte cmd = Serial.read();
+  
+  while(cmd != 'q'){
+    // Wait for command in rx buffer
+    wait_for_rx();
+    
+    byte cmd = Serial.read();
+
+    bool pump_on = false;
+    bool heater_on = false;
+    bool solenoid_on = false;
+    
+    switch(cmd){
+      case 'p':
+        // Toggle pump
+        digitalWrite(PUMP_RELAY_PIN, LOW);
+        pump_on = !pump_on;
+        break;
+      case 'h':
+        // Toggle heater
+        digitalWrite(HEATER_RELAY_PIN, LOW);
+        heater_on = !heater_on;
+        break;
+      case 's':
+        // Toggle solenoid
+        digitalWrite(SOLENOID_RELAY_PIN, LOW);
+        solenoid_on = !solenoid_on;
+        break;
+      case 'i':
+        setupPins();
+        break;
+      default:
+        Serial.println("Invalid command received!");
+        break;
+    }
+
+  }
+}
+

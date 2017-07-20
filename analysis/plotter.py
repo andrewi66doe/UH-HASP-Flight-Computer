e56fc28bd771e45d772537a5264da2504b370f9b
kdportillo <kportillo01@gmail.com>
2017-07-20 18:10:45 -0500

    cleaned up

--- analysis/plotter.py
+++ analysis/plotter.py
@@ -2,7 +2,8 @@
 # This script will create plots of all .csv in current directory
 
 import sys, datetime, os, optparse
-import pandas as pd, matplotlib as plt
+import pandas as pd
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
 	# Command line parsing 
@@ -11,33 +12,31 @@ if __name__ == "__main__":
 	(opts, args) = parser.parse_args()
 	if opts.input_file is None:
 		parser.print_help()
-		parser.error("No input file specified")
+		parser.error("No input file(s) specified")
 	else: 
 		input_file = opts.input_file
 
-	# Save only the filenames into 'filenames' list and remove the .csv part
-	# Also popping 'plotter.py' and '-i'
-	args = "\n".join(sys.argv).split()
-	args.pop(0) 
-	args.pop(0)
+	# Place each token from the command line into list but without plotter.py and -i
+	args = "\n".join(sys.argv).split()[2:]
 	filenames = [os.path.splitext(each)[0] for each in args]
 
 	# For each file, plot time vs data
-	# Title = filename
-	# x axis = time 
-	# y axis = data
+	# Title = filename, x axis = time, y axis = data
 
 	for file in filenames:
-		with open(file+'.csv', 'r', encoding='utf-8') as inFile:
+		with open(file +'.csv', 'r', encoding='utf-8') as inFile:
 			df = pd.read_csv(inFile, parse_dates=True, usecols=['Time','Data'])
-			time = pd.to_datetime(df['Time'],format='%H:%M:%S').values.tolist()
+			time = pd.to_datetime(df['Time'],format='%H:%M:%S')
+			# times = plt.dates.date2num(time)
 			data = df['Data'].values.tolist()
-
-		plt.plot(time,data,'r')
-		plt.xlabel('Time')
-		plt.ylabel('Data')
-		plt.title(file)
-		plt.savefig(file +'.png')
+			
+		for e in time:
+			print(type(e),"\t", e)
+		plt.plot_date(time,data,'r')
+		# plt.xlabel('Time')
+		# plt.ylabel('Data')
+		# plt.title(file)
+		# plt.savefig(file +'.png')
 		plt.show()
 	
 

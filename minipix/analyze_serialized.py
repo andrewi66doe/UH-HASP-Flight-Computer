e2596199ca8f242acfee51f37c4f0a8cac5291b5
Andrew <andrewg.walker@outlook.com>
2017-10-01 18:22:38 -0500

    Cleaned up and organized analysis code into functions

--- minipix/analyze_serialized.py
+++ minipix/analyze_serialized.py
@@ -50,6 +50,15 @@ def get_frame_energy(file, data):
 
 
 def get_cluster_energy(file, data):
+    cluster_types = {
+        "SMALL_BLOB": [],
+        "HEAVY_TRACK": [],
+        "HEAVY_BLOB": [],
+        "MEDIUM_BLOB": [],
+        "STRAIGHT_TRACK": [],
+        "LIGHT_TRACK": []
+    }
+
     for frame in range(file.num_frames):
         frame_data = data.get(frame, None)
 
@@ -58,10 +67,77 @@ def get_cluster_energy(file, data):
             clusters = data[frame]["clusters"]
 
             for cluster in clusters:
-                print(acq_time, end=",")
-                print(cluster[5], end=",")
-                print(cluster[2])
+                type = cluster[5]
+                energy = cluster[2]
+                cluster_types[type].append((str(acq_time), type, str(energy)))
 
+    for cluster_type in cluster_types:
+        filename = cluster_type + ".csv"
+
+        csvfile = open(filename, "w")
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow(["Timestamp", "Type", "Energy"])
+        for cluster in cluster_types[cluster_type]:
+            csvwriter.writerow(cluster)
+
+
+def get_energy_distribution():
+    cluster_types = {
+        "SMALL_BLOB": [],
+        "HEAVY_TRACK": [],
+        "HEAVY_BLOB": [],
+        "MEDIUM_BLOB": [],
+        "STRAIGHT_TRACK": [],
+        "LIGHT_TRACK": []
+    }
+
+    for frame in range(file.num_frames):
+        frame_data = data.get(frame, None)
+
+        if frame_data:
+            acq_time = data[frame]["acq_time"].replace(tzinfo=utc)
+            clusters = data[frame]["clusters"]
+
+            for cluster in clusters:
+                type = cluster[5]
+                energy = cluster[2]
+                cluster_types[type].append((str(acq_time), type, str(energy)))
+
+    for cluster_type in cluster_types:
+        filename = cluster_type + "_dist" + ".csv"
+
+        csvfile = open(filename, "w")
+        csvwriter = csv.writer(csvfile)
+
+        energy = [float(cluster[2]) for cluster in cluster_types[cluster_type]]
+        energy_distribution = normalized_distribution(energy, 50)
+
+        csvwriter.writerow(["Range", "Count"])
+        for r in energy_distribution:
+            csvwriter.writerow([r, energy_distribution[r]])
+
+
+def normalized_distribution(data, window):
+    data.sort()
+    distributed_data = {}
+    range = 0
+
+    for point in data:
+        if not distributed_data.get(range, None):
+            distributed_data[range] = 0
+
+        if point < range + window:
+            distributed_data[range] += 1
+        else:
+            range += window
+
+    # Normalize the distribution
+    total = sum(distributed_data.values())
+    for key in distributed_data:
+        distributed_data[key] /= total
+
+    return distributed_data
 
 if __name__ == "__main__":
     serialized = open('clusters.pkl', 'rb')
@@ -71,6 +147,7 @@ if __name__ == "__main__":
     file = PmfFile("HASPDATA/thurs_test36.pmf")
     file.load_dsc()
 
-    get_cluster_energy(file, data)
+    get_energy_distribution()
+    #get_cluster_energy(file, data)
     # get_cluster_count(file, data)
     # get_energy(file, data)

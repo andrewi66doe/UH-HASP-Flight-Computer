575f3cad46ee2738981da0268841162a2adfb6fd
Andrew <andrewg.walker@outlook.com>
2017-10-13 11:16:53 -0500

    Split display function up into different parts, you can now cycle through different visualization modes

--- minipix/display.py
+++ minipix/display.py
@@ -27,14 +27,31 @@ square = pygame.Surface((2, 2))
 myfont = pygame.font.SysFont("monospace", 15)
 
 
-def do_visualization(file, frame):
+def do_visualization(file, frame, mode='bb'):
     data = file.get_frame(frame)
     cluster_info = cluster_count(file, data)
+
+    if mode == 'bb':
+        render_frame(data, screen)
+        render_bb(cluster_info, screen)
+    elif mode == 'track':
+        render_frame(data, screen, fill=False)
+        render_track(cluster_info, screen)
+    elif mode == 'type':
+        render_frame(data, screen)
+        render_type(cluster_info, screen)
+
+    pygame.display.flip()
+
+    return len(cluster_info)
+
+
+def render_frame(data, screen, fill=True):
 
     for j in range(SCREEN_Y):
         for i in range(SCREEN_X):
             if data[j][i].value > int(sys.argv[3]):
-                num = 1
+                num = 1 if fill else 2
             else:
                 num = 2
             square.fill(arr[num])
@@ -42,49 +59,69 @@ def do_visualization(file, frame):
             screen.blit(square, draw_me)
     pygame.display.flip()
 
+
+def render_track(cluster_info, screen):
+    for cluster in cluster_info:
+        if cluster[7] is not None:
+            path = [np.flip(np.ceil(x), 0) for x in (cluster[7] + 1) * 2.0]
+            pygame.draw.lines(screen, WHITE, False, path)
+
+
+def render_bb(cluster_info, screen):
+
+    for cluster in cluster_info:
+        rect_points = [np.flip(np.ceil(x), 0) for x in (cluster[6] + 1) * 2.0]
+        pygame.draw.polygon(screen, RED, rect_points, 1)
+
+
+def render_type(cluster_info, screen):
+    for cluster in cluster_info:
+        y, x, _ = cluster[4]
+        label = myfont.render(str(cluster[5]), 1, (255, 255, 0))
+        screen.blit(label, ((x * 2) + 5, (y * 2) + 5))
+
+
+def render_centroid(cluster_info, screen):
     for cluster in cluster_info:
         y, x, _ = cluster[4]
         pixel = pygame.Surface((2, 2))
-        label = myfont.render(str(cluster[5]), 1, (255, 255, 0))
-        pixel.fill(WHITE)
+        pixel.fill(BLU)
         draw = pygame.Rect(x * 2, y * 2, 2, 2)
-        rect_points = [np.flip(np.ceil(x), 0) for x in (cluster[6] + 1) * 2.0]
-        #pygame.draw.lines(screen, RED, False, rect_points)
-        pygame.draw.polygon(screen, RED, rect_points, 1)
-        #pygame.draw.line(screen, RED, (x * 2, 0), (x * 2, 512))
-        #pygame.draw.line(screen, RED, (0, y * 2), (512, y * 2))
-        screen.blit(label, ((x * 2) + 5, (y * 2) + 5))
         screen.blit(pixel, draw)
-    pygame.display.flip()
-
-    return len(cluster_info)
 
 
-done = False
-frame = int(sys.argv[2])
-file = PmfFile(sys.argv[1])
-file.load_calib_a("a.txt")
-file.load_calib_b("b.txt")
-file.load_calib_c("c.txt")
-file.load_calib_t("t.txt")
+if __name__ == "__main__":
 
-clusters = do_visualization(file, frame)
+    done = False
+    frame = int(sys.argv[2])
+    file = PmfFile(sys.argv[1])
+    file.load_calib_a("a.txt")
+    file.load_calib_b("b.txt")
+    file.load_calib_c("c.txt")
+    file.load_calib_t("t.txt")
 
-while not done:
-    frame_label = myfont.render("Frame {} of {} Clusters: {}".format(frame, file.num_frames-1, clusters), 1, (255, 255, 0))
+    clusters = do_visualization(file, frame)
+    visualization_types = ['bb', 'type', 'track']
+    vtype = 0
+    while not done:
+        frame_label = myfont.render("Frame {} of {} Clusters: {}".format(frame, file.num_frames-1, clusters), 1, (255, 255, 0))
 
-    screen.blit(frame_label, (5, 0))
-    pygame.display.flip()
+        screen.blit(frame_label, (5, 0))
+        pygame.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                if frame > 0:
-                    frame -= 1
-                clusters = do_visualization(file, frame)
-            if event.key == pygame.K_RIGHT:
-                if frame < file.num_frames-1:
-                    frame += 1
-                clusters = do_visualization(file, frame)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if frame > 0:
+                        frame -= 1
+                    clusters = do_visualization(file, frame, mode=visualization_types[vtype])
+                if event.key == pygame.K_RIGHT:
+                    if frame < file.num_frames-1:
+                        frame += 1
+                    clusters = do_visualization(file, frame, mode=visualization_types[vtype])
+                if event.key == pygame.K_UP:
+                    vtype = (vtype + 1) % len(visualization_types)
+                    do_visualization(file, frame, mode=visualization_types[vtype])
+
